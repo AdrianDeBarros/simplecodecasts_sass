ecda5131ee075857c39516cac08204c1b89d9b82
AdrianBarros <a.o.debarros@gmail.com>
2015-06-19 04:07:42 +0000

    Added actionmailer settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    :port           => ENV['587'],
+    :address        => ENV['smtp.mailgun.org'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'heroku.com', #eg: 'yourappname.herokuapp.com'
+    :authentication => :plain,
+  
+  
+ 
+}
